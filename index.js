908f0e88ae5810aa7c0b1b85df349c620b8d2d31
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-11-24 18:54:49 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app =express();
+const port =5000;
+
+app.get('/',(req, res)=>{
+    res.send('Used Laptop  server is running')
+});
+
+app.listen(port, ()=>{
+    console.log(`Used Laptop server running in port=${port}`);
+})
