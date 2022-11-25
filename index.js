b8e13b692b3482ff2c876493133aaed76054fbae
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-11-24 22:00:02 -0800

    add env and midleware

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 const express = require('express');
-const app =express();
-const port =5000;
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { query } = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
+
+const express = require('express');
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+//user: user6
+//pass: PD6C7m8trWXK1XfT
+
+
+
+
+const uri = "mongodb+srv://user6:PD6C7m8trWXK1XfT@cluster0.ltjdg3f.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run(){
+
+    try{
+
+         const serviceCollection = client.db('ServiceCollection').collection('services');
+         const reviewCollection = client.db('ServiceCollection').collection('reviews');
+  
+
+         app.post('/addproduct', async(req, res) =>{
+             const service = req.body;
+             console.log(service); 
+             const result = await serviceCollection.insertOne(service);
+             res.send(result);
+         })
+
+    }
+
+    finally{
+
+    }
+
+}
+
+run().catch(err => console.log(err));
 
 
 app.get('/',(req, res)=>{
